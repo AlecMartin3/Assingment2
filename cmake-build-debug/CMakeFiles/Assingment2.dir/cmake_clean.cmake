940d0ab2d1d637409a3a5a3b6694953284ab92aa
Alec Martin <acmartin1@live.ca>
2019-12-01 12:07:24 -0800

    Initial

---
+++ cmake-build-debug/CMakeFiles/Assingment2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assingment2.dir/main.cpp.o"
+  "Assingment2.pdb"
+  "Assingment2.exe"
+  "libAssingment2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assingment2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
